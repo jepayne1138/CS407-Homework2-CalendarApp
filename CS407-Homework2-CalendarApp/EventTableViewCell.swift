b997beefa26dd336ff29483b7d1ea7f26105adad
James Payne <jim@jamess-macbook-pro.local>
2015-10-25 12:40:25 -0500

    Lay out all elements.  Set auto-layout constraints.  Create custom controllers for each view.

---
+++ CS407-Homework2-CalendarApp/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  CS407-Homework2-CalendarApp
+//
+//  Created by James Payne on 10/25/15.
+//  Copyright © 2015 James Payne. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Event.swift
@@ -0,0 +1,16 @@
+//
+//  Event.swift
+//  CS407-Homework2-CalendarApp
+//
+//  Created by James Payne on 10/24/15.
+//  Copyright © 2015 James Payne. All rights reserved.
+//
+
+import Foundation
+
+struct Event {
+    var name: String
+    var start: NSDate
+    var end: NSDate
+    var description: String
+}
